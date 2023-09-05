266b33817b19a39faf4fb3b7c7a456973d13c6b5
LPreche <paulorecheme@hotmail.com>
2023-09-04 21:19:49 -0300

    feat: Retirado botão de stats e alterado para abrir o detalhamento dos stats com um clique em qualquer local do card do pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,25 +8,19 @@ let offset = 0;
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map((pokemon) => `
-        
-        <li class="pokemon ${pokemon.type}" >
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-            
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-                <img src="${pokemon.photo}" alt="${pokemon.name}">
-            </div>
-            <div class="pokemonInfo">
-                <a href="./pokemon-stats.html?id=${pokemon.number}">
-                    <button id="pokemonInfoButton" type="button">
-                        Stats
-                    </button>
-                </a>
-            </div>
-        </li>
+        <a class="pokemonInfo" href="./pokemon-stats.html?id=${pokemon.number}">
+            <li class="pokemon ${pokemon.type}" >
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+        </a>
 
         `).join('')
 
